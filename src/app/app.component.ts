d70d30fd4f17c133fc413751408022168f5d4f98
Vladyslav <139408781+Vladyslav-Skvortsov@users.noreply.github.com>
2024-06-10 15:05:52 +0300

    add mock data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,46 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'MovieApp';
+  movies = [
+    {
+      image: '../assets/img/mock-img.jpg',
+      id: 129590,
+      overview:
+        'In the near future, a group of war journalists attempt to survive while reporting the truth as the United States stands on the brink of civil war.',
+      title: 'Movie 1',
+      rating: 8.5,
+    },
+    {
+      image: '../assets/img/mock-img.jpg',
+      id: 329590,
+      overview:
+        'In the near future, a group of war journalists attempt to survive while reporting the truth as the United States stands on the brink of civil war.',
+      title: 'Movie 2',
+      rating: 6.5,
+    },
+    {
+      image: '../assets/img/mock-img.jpg',
+      id: 529590,
+      overview:
+        'In the near future, a group of war journalists attempt to survive while reporting the truth as the United States stands on the brink of civil war.',
+      title: 'Movie 3',
+      rating: 7.5,
+    },
+    {
+      image: '../assets/img/mock-img.jpg',
+      id: 959999,
+      overview:
+        'In the near future, a group of war journalists attempt to survive while reporting the truth as the United States stands on the brink of civil war.',
+      title: 'Movie 3',
+      rating: 5.5,
+    },
+  ];
+
+  // favoriteMovies: any[] = [];
+  // watcMovies: any[] = [];
+
+
 }
