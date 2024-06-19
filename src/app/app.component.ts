024b8f3b08a35d133fd7d6d294d56fa9a42414cb
Vladyslav <139408781+Vladyslav-Skvortsov@users.noreply.github.com>
2024-06-19 04:01:36 +0300

    add conf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,66 +4,66 @@ import { MovieListComponent } from './components/movie-list/movie-list.component
 import { NgIf } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, MovieListComponent, NgIf],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, MovieListComponent, NgIf],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  movies = [
-    {
-      image: '../assets/img/mock-img.jpg',
-      id: 129590,
-      overview:
-        'In the near future, a group of war journalists attempt to survive while reporting the truth as the United States stands on the brink of civil war.',
-      title: 'Movie 1',
-      rating: 8.0,
-    },
-    {
-      image: '../assets/img/mock-img.jpg',
-      id: 329590,
-      overview:
-        'In the near future, a group of war journalists attempt to survive while reporting the truth as the United States stands on the brink of civil war.',
-      title: 'Movie 2',
-      rating: 6.5,
-    },
-    {
-      image: '../assets/img/mock-img.jpg',
-      id: 529590,
-      overview:
-        'In the near future, a group of war journalists attempt to survive while reporting the truth as the United States stands on the brink of civil war.',
-      title: 'Movie 3',
-      rating: 7.0,
-    },
-    {
-      image: '../assets/img/mock-img.jpg',
-      id: 959999,
-      overview:
-        'In the near future, a group of war journalists attempt to survive while reporting the truth as the United States stands on the brink of civil war.',
-      title: 'Movie 4',
-      rating: 5.5,
-    },
-  ];
+	movies = [
+		{
+			image: '../assets/img/mock-img.jpg',
+			id: 129590,
+			overview:
+				'In the near future, a group of war journalists attempt to survive while reporting the truth as the United States stands on the brink of civil war.',
+			title: 'Movie 1',
+			rating: 8.0,
+		},
+		{
+			image: '../assets/img/mock-img.jpg',
+			id: 329590,
+			overview:
+				'In the near future, a group of war journalists attempt to survive while reporting the truth as the United States stands on the brink of civil war.',
+			title: 'Movie 2',
+			rating: 6.5,
+		},
+		{
+			image: '../assets/img/mock-img.jpg',
+			id: 529590,
+			overview:
+				'In the near future, a group of war journalists attempt to survive while reporting the truth as the United States stands on the brink of civil war.',
+			title: 'Movie 3',
+			rating: 7.0,
+		},
+		{
+			image: '../assets/img/mock-img.jpg',
+			id: 959999,
+			overview:
+				'In the near future, a group of war journalists attempt to survive while reporting the truth as the United States stands on the brink of civil war.',
+			title: 'Movie 4',
+			rating: 5.5,
+		},
+	];
 
-  favoriteMovies: any[] = [];
-  watchMovies: any[] = [];
+	favoriteMovies: any[] = [];
+	watchMovies: any[] = [];
 
-  addFavorite(movie: any) {
-    const isMovieInFavorite = this.favoriteMovies.some(
-      (favoriteMovie) => favoriteMovie.id === movie.id
-    );
-    if (!isMovieInFavorite) {
-      this.favoriteMovies.push(movie);
-    }
-  }
+	addFavorite(movie: any) {
+		const isMovieInFavorite = this.favoriteMovies.some(
+			(favoriteMovie) => favoriteMovie.id === movie.id
+		);
+		if (!isMovieInFavorite) {
+			this.favoriteMovies.push(movie);
+		}
+	}
 
-  addWatch(movie: any) {
-    const isMovieInWatch = this.watchMovies.some(
-      (watchMovie) => watchMovie.id === movie.id
-    );
-    if (!isMovieInWatch) {
-      this.watchMovies.push(movie);
-    }
-  }
+	addWatch(movie: any) {
+		const isMovieInWatch = this.watchMovies.some(
+			(watchMovie) => watchMovie.id === movie.id
+		);
+		if (!isMovieInWatch) {
+			this.watchMovies.push(movie);
+		}
+	}
 }
--- src/app/components/movie-item/movie-item.component.spec.ts
+++ src/app/components/movie-item/movie-item.component.spec.ts
@@ -1,5 +1,3 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MovieItemComponent } from './movie-item.component';
-
-
