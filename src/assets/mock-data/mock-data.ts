1cc9dceb45cebd5da23fe85f7f3ed6b743f7a2ac
Vladyslav <139408781+Vladyslav-Skvortsov@users.noreply.github.com>
2024-06-22 11:01:18 +0300

    fix mock data

--- src/assets/mock-data/mock-data.ts
+++ src/assets/mock-data/mock-data.ts
@@ -9,7 +9,7 @@ export const nowPlayingMovies = [
 		overview:
 			"Teenager Riley's mind headquarters is undergoing a sudden demolition to make room for something entirely unexpected: new Emotions! Joy, Sadness, Anger, Fear and Disgust, who’ve long been running a successful operation by all accounts, aren’t sure how to feel when Anxiety shows up. And it looks like she’s not alone.",
 		popularity: 9750.804,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-06-11',
 		title: 'Inside Out 2',
 		video: false,
@@ -26,7 +26,7 @@ export const nowPlayingMovies = [
 		overview:
 			"Several generations in the future following Caesar's reign, apes are now the dominant species and live harmoniously while humans have been reduced to living in the shadows. As a new tyrannical ape leader builds his empire, one young ape undertakes a harrowing journey that will cause him to question all that he has known about the past and to make choices that will define a future for apes and humans alike.",
 		popularity: 4014.879,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-05-08',
 		title: 'Kingdom of the Planet of the Apes',
 		video: false,
@@ -43,7 +43,7 @@ export const nowPlayingMovies = [
 		overview:
 			'In the Summer of 2024, Paris is hosting the World Triathlon Championships on the Seine for the first time. Sophia, a brilliant scientist, learns from Mika, a young environmental activist, that a large shark is swimming deep in the river. To avoid a bloodbath at the heart of the city, they have no choice but to join forces with Adil, the Seine river police commander.',
 		popularity: 2822.686,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-06-05',
 		title: 'Under Paris',
 		video: false,
@@ -60,7 +60,7 @@ export const nowPlayingMovies = [
 		overview:
 			'After their late former Captain is framed, Lowrey and Burnett try to clear his name, only to end up on the run themselves.',
 		popularity: 2744.583,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-06-05',
 		title: 'Bad Boys: Ride or Die',
 		video: false,
@@ -77,7 +77,7 @@ export const nowPlayingMovies = [
 		overview:
 			'In the near future, a group of war journalists attempt to survive while reporting the truth as the United States stands on the brink of civil war.',
 		popularity: 1707.985,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-04-10',
 		title: 'Civil War',
 		video: false,
@@ -94,7 +94,7 @@ export const nowPlayingMovies = [
 		overview:
 			'When Karate champion Michael Rivers wins the last tournament of his career, shady businessman Ron Hall offers him the opportunity to fight in an illegal Kumite in Bulgaria against the world’s best martial artists.  When Michael declines, Hall has his daughter kidnapped and, in order to rescue her, Rivers is left with no choice but to compete in the deadly tournament. Arriving in Bulgaria, he finds out that he is not the only fighter whose loved one was taken. Rivers enlists the help of trainers Master Loren, and Julie Jackson but will it be enough for him to win the tournament and save his daughter’s life?',
 		popularity: 1246.18,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-05-09',
 		title: 'The Last Kumite',
 		video: false,
@@ -111,7 +111,7 @@ export const nowPlayingMovies = [
 		overview:
 			'A brilliant counterterrorism analyst with a deep distrust of AI discovers it might be her only hope when a mission to capture a renegade robot goes awry.',
 		popularity: 1161.652,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-05-23',
 		title: 'Atlas',
 		video: false,
@@ -131,7 +131,7 @@ export const popularMovies = [
 		overview:
 			"Teenager Riley's mind headquarters is undergoing a sudden demolition to make room for something entirely unexpected: new Emotions! Joy, Sadness, Anger, Fear and Disgust, who’ve long been running a successful operation by all accounts, aren’t sure how to feel when Anxiety shows up. And it looks like she’s not alone.",
 		popularity: 9750.804,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-06-11',
 		title: 'Inside Out 2',
 		video: false,
@@ -148,7 +148,7 @@ export const popularMovies = [
 		overview:
 			"Several generations in the future following Caesar's reign, apes are now the dominant species and live harmoniously while humans have been reduced to living in the shadows. As a new tyrannical ape leader builds his empire, one young ape undertakes a harrowing journey that will cause him to question all that he has known about the past and to make choices that will define a future for apes and humans alike.",
 		popularity: 4014.879,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-05-08',
 		title: 'Kingdom of the Planet of the Apes',
 		video: false,
@@ -165,7 +165,7 @@ export const popularMovies = [
 		overview:
 			'In the Summer of 2024, Paris is hosting the World Triathlon Championships on the Seine for the first time. Sophia, a brilliant scientist, learns from Mika, a young environmental activist, that a large shark is swimming deep in the river. To avoid a bloodbath at the heart of the city, they have no choice but to join forces with Adil, the Seine river police commander.',
 		popularity: 2822.686,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-06-05',
 		title: 'Under Paris',
 		video: false,
@@ -182,7 +182,7 @@ export const popularMovies = [
 		overview:
 			'After their late former Captain is framed, Lowrey and Burnett try to clear his name, only to end up on the run themselves.',
 		popularity: 2744.583,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-06-05',
 		title: 'Bad Boys: Ride or Die',
 		video: false,
@@ -199,7 +199,7 @@ export const popularMovies = [
 		overview:
 			'From an adventurous balloon ride above the clouds to a monster-filled metropolis, Academy Award®-winning director Pete Docter (“Monsters, Inc.,” “Up”) has taken audiences to unique and imaginative places. In Disney and Pixar’s original movie “ Inside Out,” he will take us to the most extraordinary location of all—inside the mind.',
 		popularity: 2731.637,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2015-06-17',
 		title: 'Inside Out',
 		video: false,
@@ -216,7 +216,7 @@ export const popularMovies = [
 		overview:
 			'Following their explosive showdown, Godzilla and Kong must reunite against a colossal undiscovered threat hidden within our world, challenging their very existence – and our own.',
 		popularity: 2007.024,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-03-27',
 		title: 'Godzilla x Kong: The New Empire',
 		video: false,
@@ -233,7 +233,7 @@ export const popularMovies = [
 		overview:
 			'In the near future, a group of war journalists attempt to survive while reporting the truth as the United States stands on the brink of civil war.',
 		popularity: 1707.985,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-04-10',
 		title: 'Civil War',
 		video: false,
@@ -250,7 +250,7 @@ export const popularMovies = [
 		overview:
 			'Detective Ma Seok-do changes his affiliation from the Geumcheon Police Station to the Metropolitan Investigation Team, in order to eradicate Japanese gangsters who enter Korea to commit heinous crimes.',
 		popularity: 1415.101,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2023-05-31',
 		title: 'The Roundup: No Way Out',
 		video: false,
@@ -270,7 +270,7 @@ export const topRatedMovies = [
 		overview:
 			'Imprisoned in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.',
 		popularity: 134.639,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '1994-09-23',
 		title: 'The Shawshank Redemption',
 		video: false,
@@ -287,7 +287,7 @@ export const topRatedMovies = [
 		overview:
 			'Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family patriarch, Vito Corleone barely survives an attempt on his life, his youngest son, Michael steps in to take care of the would-be killers, launching a campaign of bloody revenge.',
 		popularity: 115.64,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '1972-03-14',
 		title: 'The Godfather',
 		video: false,
@@ -304,7 +304,7 @@ export const topRatedMovies = [
 		overview:
 			'In the continuing saga of the Corleone crime family, a young Vito Corleone grows up in Sicily and in 1910s New York. In the 1950s, Michael Corleone attempts to expand the family business into Las Vegas, Hollywood and Cuba.',
 		popularity: 106.361,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '1974-12-20',
 		title: 'The Godfather Part II',
 		video: false,
@@ -321,7 +321,7 @@ export const topRatedMovies = [
 		overview:
 			'The true story of how businessman Oskar Schindler saved over a thousand Jewish lives from the Nazis while they worked as slaves in his factory during World War II.',
 		popularity: 66.619,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '1993-12-15',
 		title: "Schindler's List",
 		video: false,
@@ -338,7 +338,7 @@ export const topRatedMovies = [
 		overview:
 			"The defense and the prosecution have rested and the jury is filing into the jury room to decide if a young Spanish-American is guilty or innocent of murdering his father. What begins as an open and shut case soon becomes a mini-drama of each of the jurors' prejudices and preconceptions about the trial, the accused, and each other.",
 		popularity: 64.799,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '1957-04-10',
 		title: '12 Angry Men',
 		video: false,
@@ -355,7 +355,7 @@ export const topRatedMovies = [
 		overview:
 			'Raj is a rich, carefree, happy-go-lucky second generation NRI. Simran is the daughter of Chaudhary Baldev Singh, who in spite of being an NRI is very strict about adherence to Indian values. Simran has left for India to be married to her childhood fiancé. Raj leaves for India with a mission at his hands, to claim his lady love under the noses of her whole family. Thus begins a saga.',
 		popularity: 29.108,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '1995-10-20',
 		title: 'Dilwale Dulhania Le Jayenge',
 		video: false,
@@ -372,7 +372,7 @@ export const topRatedMovies = [
 		overview:
 			'A young girl, Chihiro, becomes trapped in a strange new world of spirits. When her parents undergo a mysterious transformation, she must call upon the courage she never knew she had to free her family.',
 		popularity: 107.431,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2001-07-20',
 		title: 'Spirited Away',
 		video: false,
@@ -389,7 +389,7 @@ export const topRatedMovies = [
 		overview:
 			'Batman raises the stakes in his war on crime. With the help of Lt. Jim Gordon and District Attorney Harvey Dent, Batman sets out to dismantle the remaining criminal organizations that plague the streets. The partnership proves to be effective, but they soon find themselves prey to a reign of chaos unleashed by a rising criminal mastermind known to the terrified citizens of Gotham as the Joker.',
 		popularity: 101.049,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2008-07-16',
 		title: 'The Dark Knight',
 		video: false,
@@ -406,7 +406,7 @@ export const topRatedMovies = [
 		overview:
 			"All unemployed, Ki-taek's family takes peculiar interest in the wealthy and glamorous Parks for their livelihood until they get entangled in an unexpected incident.",
 		popularity: 86.976,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2019-05-30',
 		title: 'Parasite',
 		video: false,
@@ -423,7 +423,7 @@ export const topRatedMovies = [
 		overview:
 			"A supernatural tale set on death row in a Southern prison, where gentle giant John Coffey possesses the mysterious power to heal people's ailments. When the cell block's head guard, Paul Edgecomb, recognizes Coffey's miraculous gift, he tries desperately to help stave off the condemned man's execution.",
 		popularity: 74.976,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '1999-12-10',
 		title: 'The Green Mile',
 		video: false,
@@ -443,7 +443,7 @@ export const upcomingMovies = [
 		overview:
 			"Teenager Riley's mind headquarters is undergoing a sudden demolition to make room for something entirely unexpected: new Emotions! Joy, Sadness, Anger, Fear and Disgust, who’ve long been running a successful operation by all accounts, aren’t sure how to feel when Anxiety shows up. And it looks like she’s not alone.",
 		popularity: 9750.804,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-06-11',
 		title: 'Inside Out 2',
 		video: false,
@@ -460,7 +460,7 @@ export const upcomingMovies = [
 		overview:
 			'After their late former Captain is framed, Lowrey and Burnett try to clear his name, only to end up on the run themselves.',
 		popularity: 2744.583,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-06-05',
 		title: 'Bad Boys: Ride or Die',
 		video: false,
@@ -477,7 +477,7 @@ export const upcomingMovies = [
 		overview:
 			'A young artist gets stranded in an extensive, immaculate forest in western Ireland, where, after finding shelter, she becomes trapped alongside three strangers, stalked by mysterious creatures each night.',
 		popularity: 1117.26,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-06-06',
 		title: 'The Watchers',
 		video: false,
@@ -494,7 +494,7 @@ export const upcomingMovies = [
 		overview:
 			'A mild-mannered professor moonlighting as a fake hit man in police stings ignites a chain reaction of trouble when he falls for a potential client.',
 		popularity: 832.05,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-05-16',
 		title: 'Hit Man',
 		video: false,
@@ -511,7 +511,7 @@ export const upcomingMovies = [
 		overview:
 			'Garfield, the world-famous, Monday-hating, lasagna-loving indoor cat, is about to have a wild outdoor adventure! After an unexpected reunion with his long-lost father – scruffy street cat Vic – Garfield and his canine friend Odie are forced from their perfectly pampered life into joining Vic in a hilarious, high-stakes heist.',
 		popularity: 680.296,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-04-30',
 		title: 'The Garfield Movie',
 		video: false,
@@ -528,7 +528,7 @@ export const upcomingMovies = [
 		overview:
 			'An American nun embarks on a new journey when she joins a remote convent in the Italian countryside. However, her warm welcome quickly turns into a living nightmare when she discovers her new home harbours a sinister secret and unspeakable horrors.',
 		popularity: 509.32,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-03-20',
 		title: 'Immaculate',
 		video: false,
@@ -545,7 +545,7 @@ export const upcomingMovies = [
 		overview:
 			'When his family is murdered, a deaf-mute named Boy escapes to the jungle and is trained by a mysterious shaman to repress his childish imagination and become an instrument of death.',
 		popularity: 427.268,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-04-24',
 		title: 'Boy Kills World',
 		video: false,
@@ -562,7 +562,7 @@ export const upcomingMovies = [
 		overview:
 			'In C.E.75, the fighting still continues. There are independence movements, and aggression by Blue Cosmos... In order to calm the situation, a global peace monitoring agency called COMPASS is established, with Lacus as its first president. As members of COMPASS, Kira and his comrades intervene into various regional battles. Then a newly established nation called Foundation proposes a joint operation against a Blue Cosmos stronghold.',
 		popularity: 375.399,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-01-26',
 		title: 'Mobile Suit Gundam SEED FREEDOM',
 		video: false,
@@ -579,7 +579,7 @@ export const upcomingMovies = [
 		overview:
 			'Gru and Lucy and their girls — Margo, Edith and Agnes — welcome a new member to the Gru family, Gru Jr., who is intent on tormenting his dad. Gru faces a new nemesis in Maxime Le Mal and his femme fatale girlfriend Valentina, and the family is forced to go on the run.',
 		popularity: 373.149,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-06-20',
 		title: 'Despicable Me 4',
 		video: false,
@@ -596,7 +596,7 @@ export const upcomingMovies = [
 		overview:
 			'After learning that the death of his wife was not an accident, a former CIA Station Chief is forced back into the espionage underworld, teaming up with an adversary to unravel a conspiracy that challenges everything he thought he knew.',
 		popularity: 351.28,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-05-02',
 		title: 'Chief of Station',
 		video: false,
@@ -613,7 +613,7 @@ export const upcomingMovies = [
 		overview:
 			"After raising an unnervingly talented spider in secret, 12-year-old Charlotte must face the truth about her pet and fight for her family's survival.",
 		popularity: 293.651,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2024-04-12',
 		title: 'Sting',
 		video: false,
@@ -630,7 +630,7 @@ export const upcomingMovies = [
 		overview:
 			'Based on the most well-known classical fantasy novel of China, Fengshenyanyi, the trilogy is a magnificent eastern high fantasy epic that recreates the prolonged mythical wars between humans, immortals and monsters, which happened more than three thousand years ago.',
 		popularity: 234.995,
-		poster_path: '../../../assets/img/mock-img.jpg',
+		poster_path: 'assets/img/mock-img.jpg',
 		release_date: '2023-07-20',
 		title: 'Creation of the Gods I: Kingdom of Storms',
 		video: false,
