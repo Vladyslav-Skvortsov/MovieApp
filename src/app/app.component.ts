4b4d938149a4f7414eaf889e734ec6507db28d18
Vladyslav <139408781+Vladyslav-Skvortsov@users.noreply.github.com>
2024-06-25 15:08:44 +0300

    another mini fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, RouterModule, RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { MovieListComponent } from '@components/movie-list/movie-list.component';
 import { MovieCardComponent } from '@components/movie-card/movie-card.component';
 import { SidebarComponent } from '@components/sidebar/sidebar.component';
-import { Movie } from '@interfaces/movie';
 import { HeaderComponent } from './components/header/header.component';
 import { MovieDetailComponent } from './pages/movie-detail/movie-detail.component';
 
