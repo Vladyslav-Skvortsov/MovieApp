85def082fa9365efa844273cf872238cdae7ab31
Vladyslav <139408781+Vladyslav-Skvortsov@users.noreply.github.com>
2024-07-15 06:43:06 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-	providers: [
-		provideHttpClient(),
-		provideRouter(routes, withComponentInputBinding()),
-	],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+	console.error(err)
+);
